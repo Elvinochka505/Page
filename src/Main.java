40a57418b1ef0d768e29b61e067eeb414f086361
Elvina <khabibullinaelvina@mail.ru>
2025-03-15 15:59:03 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int amount = 300;
+        int plus = 1_100;
+        int bonus;
+        if (plus >= 1000) {
+            bonus = plus / 100;
+        } else {
+            bonus = 0;
+    }
+
+        System.out.println(bonus);
+        System.out.println (amount+plus+bonus);
+    }
+}
